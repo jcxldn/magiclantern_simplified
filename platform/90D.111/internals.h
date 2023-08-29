e4a9674fc4687893e751ab2d4c43877539d9d4d4
James Cahill <jcahill773@gmail.com>
2023-08-29 16:53:43 +0000

    (90D.111/internals): Add missing defines from 850D

--- platform/90D.111/internals.h
+++ platform/90D.111/internals.h
@@ -5,6 +5,8 @@
 // This camera has a DIGIC VIII chip
 #define CONFIG_DIGIC_VIII
 
+#define CONFIG_MMU
+
 // Digic 8 does not have bitmap font in ROM, try to load it from card
 #define CONFIG_NO_BFNT
 
@@ -12,8 +14,12 @@
 #define CONFIG_LIVEVIEW
 
 // enable state objects hooks
-//#define CONFIG_STATE_OBJECT_HOOKS
+#define CONFIG_STATE_OBJECT_HOOKS
 
 // Large total memory, leading to unusual memory mapping,
 // CACHEABLE / UNCACHEABLE changes
 #define CONFIG_MEM_2GB
+
+#define CONFIG_NEW_TASK_STRUCTS
+#define CONFIG_TASK_STRUCT_V2_SMP
+#define CONFIG_TASK_ATTR_STRUCT_V5
